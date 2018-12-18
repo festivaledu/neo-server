b786985b06d2610519586f10e23ea8020ae6fa8f
Väinämö Łūmikērø <vainamo@git.festival.ml>
2018-12-18 17:23:27 +0100

    Added basic server functionality for festivaledu/neo#1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Neo.Server
+{
+    internal class Program
+    {
+        private static readonly NeoServer server = new NeoServer();
+
+        internal static void Main(string[] args) {
+            server.Register();
+            server.Start();
+            Console.ReadLine();
+            server.Stop();
+        }
+    }
+}
