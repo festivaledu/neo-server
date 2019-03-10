0d3c8fe84be64e0a2d63c5c843cb3dd4fd7f76c9
Väinämö Łūmikērø <vainamo@git.festival.ml>
2019-03-10 13:46:38 +0100

    Cleanup path name fixes

--- Program.cs
+++ Program.cs
@@ -1,28 +1,26 @@
 ﻿using System;
 using System.IO;
-using Neo.Core.Extensibility;
 
 namespace Neo.Server
 {
     internal class Program
     {
         private static readonly NeoServer server = new NeoServer();
-		
-		private static readonly String configPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"config.json");
-		private static readonly String dataPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"data");
-		private static readonly String pluginPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"plugins");
-		
+
+        private static readonly string configPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"config.json");
+        private static readonly string dataPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"data");
+        private static readonly string pluginPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"plugins");
 
         internal static void Main(string[] args) {
-			if (!Directory.Exists(dataPath)) {
-				Directory.CreateDirectory(dataPath);
-			}
-			if (!Directory.Exists(pluginPath)) {
-				Directory.CreateDirectory(pluginPath);
-			}
-			
+            if (!Directory.Exists(dataPath)) {
+                Directory.CreateDirectory(dataPath);
+            }
+            if (!Directory.Exists(pluginPath)) {
+                Directory.CreateDirectory(pluginPath);
+            }
+
             server.Initialize(configPath, dataPath, pluginPath);
-            
+
             server.Start();
 
             Console.ReadLine();
