e0d54350b9211294b0f1230eb3b933f34c5ac501
Väinämö Łūmikērø <vainamo@git.festival.ml>
2019-03-11 10:08:23 +0100

    Add web server for avatar serving

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using Neo.Core.Networking;
 
 namespace Neo.Server
 {
@@ -10,21 +11,30 @@ namespace Neo.Server
         private static readonly string configPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"config.json");
         private static readonly string dataPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"data");
         private static readonly string pluginPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"plugins");
+        private static readonly string avatarPath = Path.Combine(dataPath, @"avatars");
 
         internal static void Main(string[] args) {
             if (!Directory.Exists(dataPath)) {
                 Directory.CreateDirectory(dataPath);
             }
+
             if (!Directory.Exists(pluginPath)) {
                 Directory.CreateDirectory(pluginPath);
             }
+            
+            if (!Directory.Exists(avatarPath)) {
+                Directory.CreateDirectory(avatarPath);
+            }
 
             server.Initialize(configPath, dataPath, pluginPath);
-
             server.Start();
 
+            var webServer = new WebServer(avatarPath, 43430);
+
             Console.ReadLine();
+
             server.Stop();
+            webServer.Stop();
 
             Console.ReadLine();
         }
